45f54092a59daba26426096b534caf868a0fcebd
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-08-21 04:53:06 +0300

    React update

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+    <button className="btn1">Button 1</button>
+
+    <button className="btn2">Button 2</button>
+
+    <button className="btn3">Button 3</button>
+    </>
+  );
+}
